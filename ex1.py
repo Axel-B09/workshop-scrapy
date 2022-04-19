99b3247d4eff6fc79a7349fa7deea276b3bc416c
River <axel.boqueho@epitech.eu>
2022-04-19 14:26:46 +0200

    [add] ex1

---
+++ ex1.py
@@ -0,0 +1,23 @@
+##
+## EPITECH PROJECT, 2022
+## workshop-scrapy
+## File description:
+## ex1
+##
+
+import scrapy
+
+
+nblist = 1;
+check = 0;
+
+while nblist <= 100:
+    if (nblist % 3 == 0 and nblist % 5 == 0):
+        print("ThreeFive");
+    elif (nblist % 5 == 0):
+        print("Five");
+    elif (nblist % 3 == 0):
+        print("Three");
+    else:
+        print(nblist);
+    nblist+=1;
